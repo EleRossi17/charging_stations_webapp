73bab0f9a42461d61cff9104b2df970abf547f16
AngeloBerardi <angeloberardi96@gmail.com>
2025-07-31 13:02:43 +0200

    slider implementato

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
+from fastapi.responses import JSONResponse
+import pandas as pd
+import os
 from data_loader import load_stations
 
 app = FastAPI()
@@ -15,3 +18,14 @@ async def index():
 @app.get("/stations")
 def stations():
     return load_stations()
+
+@app.get("/api/stations")
+def get_stations():
+    path = "data/as_is"
+    dfs = []
+    for file in os.listdir(path):
+        if file.endswith(".csv"):
+            df = pd.read_csv(os.path.join(path, file))
+            dfs.append(df)
+    merged_df = pd.concat(dfs)
+    return JSONResponse(content=merged_df.to_dict(orient="records"))
